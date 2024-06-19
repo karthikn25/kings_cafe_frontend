50268c6632984fad75c26a1bec11285d726591db
karthikn25 <mkarthikeyan179@gmail.com>
2024-06-19 08:56:25 +0530

    first commit

--- src/Users/Forget/Forget.js
+++ src/Users/Forget/Forget.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Forget.css'
+import { URL } from '../../Server';
 
 export default function Forget() {
+  const [email,setEmail]=useState();
+
+  const [error,setError]=useState();
+  const [success,setSuccess]=useState();
+
+  const handleForget = async(e)=>{
+    e.preventDefault();
+
+    const res = await fetch(`${URL}/user/forget-password`,{
+      method:"POST",
+      body:JSON.stringify({email}),
+      headers:{
+        'Content-Type':'application/json'
+      }
+    })
+    const data = await res.json();
+    console.log(data);
+    if(!data.link){
+      setError("Error occured in Email Send")
+      setSuccess("")
+    }else{
+      setSuccess("Mail send Successfully")
+      setError('')
+    }
+  }
   return (
     <div id='forget'>
     <div className="row" id='forget-form-row'>
@@ -14,13 +40,18 @@ export default function Forget() {
       className="form-control"
       id="floatingInput"
       placeholder="johndoe@gmail.com"
+      value={email}
+      onChange={(e)=>setEmail(e.target.value)}
     />
     <label for="floatingInput">Email</label>
   </div>
     </form>
     <div id='forget-btn'>
-    <button>UPDATE</button>
+    <button onClick={handleForget}>UPDATE</button>
     </div>
+    {
+      error ? <div id='error'>{error}</div> : <div id='success'>{success}</div>
+    }
     </div>
     </div>
     
--- src/Users/Reset/Reset.js
+++ src/Users/Reset/Reset.js
@@ -1,51 +1,101 @@
-import React, { useState } from 'react'
-import './Reset.css'
+import React, { useState } from "react";
+import "./Reset.css";
+import { URL } from "../../Server";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function Reset() {
-  const [show,setShow]=useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+  const [error, setError] = useState();
+  const [success, setSuccess] = useState();
+  const [show, setShow] = useState();
 
-  const toggle = ()=>setShow(!show);
+  const navigate = useNavigate();
+
+  const {token,id} = useParams();
+  console.log(id);
+
+  const toggle = () => setShow(!show);
+
+  const handleReset = async (e) => {
+    const userData = {
+      password
+    }
+    e.preventDefault();
+    if (password === confirmPassword) {
+      
+      const res = await fetch(`${URL}/user/reset-password/${id}/${token}`, {
+        method: "PUT",
+        body: JSON.stringify(userData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      console.log(data);
+      if (!data.status) {
+        setError(data.message);
+        setSuccess("");
+      } else {
+        setSuccess(data.message);
+        setError("");
+        setTimeout(() => {
+          navigate("/");
+        },2000);
+      }
+    } else {
+      setError("Wrong Password");
+      setSuccess("");
+    }
+  };
 
   return (
-    <div id='reset'>
-    <div className='row'>
-    <div className='col' id='reset-msg'>
-    <h3>CHANGE YOUR</h3>
-    <h6>DELICIOUS FOOD</h6>
-    </div>
-    </div>
-    <div className='row'>
-    <div className='col' id='reset-form'>
-    <div id='reset-form-field'>
-    <div className="form-floating" id="reset-field">
+    <div id="reset">
+      <div className="row">
+        <div className="col" id="reset-msg">
+          <h3>CHANGE YOUR</h3>
+          <h6>DELICIOUS FOOD</h6>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col" id="reset-form">
+          <div id="reset-form-field">
+            <div className="form-floating" id="reset-field">
               <input
                 type={!show ? "password" : "text"}
                 className="form-control"
                 id="floatingPassword"
                 placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
               <label for="floatingPassword">Password</label>
             </div>
             <div className="reset-show">
-              <input type="checkbox" onClick={toggle}/> Show
+              <input type="checkbox" onClick={toggle} /> Show
             </div>
             <div className="form-floating" id="reset-field">
-            <input
-              type= "password"
-              className="form-control"
-              id="floatingPassword"
-              placeholder="Confirm Password"
-            />
-            <label for="floatingPassword">Password</label>
-          </div> 
-         
+              <input
+                type="password"
+                className="form-control"
+                id="floatingPassword"
+                placeholder="Confirm Password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              <label for="floatingPassword">Password</label>
+            </div>
+          </div>
+        </div>
+        <div id="reset-btn">
+          <button onClick={handleReset}>CHANGE</button>
+        </div>
+        {error ? (
+          <div id="error">{error}</div>
+        ) : (
+          <div id="success">{success}</div>
+        )}
+      </div>
     </div>
-    
-    </div>
-    <div id='reset-btn'>
-    <button>CHANGE</button>
-    </div>
-    </div>
-    </div>
-  )
+  );
 }
